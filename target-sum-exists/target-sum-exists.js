620bc830b74a2717a578e616b10c7cde116022f3
Leesel Fraser <leeselfraser08@gmail.com>
2020-11-20 18:01:33 -0500

    complete three sum

--- target-sum-exists/target-sum-exists.js
+++ target-sum-exists/target-sum-exists.js
@@ -10,8 +10,24 @@ const targetSumExistsInTwoNumbers = (list, target) => {
 }
 
 const targetSumExistsInThreeNumbers = (list, target) => {
-  return false
+  let numObject = {};
+  for (let i = 0; i < list.length; i++) {
+    let thisNum = list[i];
+    numObject[thisNum] = i;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    let diff = target - list[i];
+    if (numObject.hasOwnProperty(diff) && numObject[diff] !== i) {
+    	return true;
+    }
+  }
+  
+  return false;
 }
+
+console.log(targetSumExistsInThreeNumbers([2, 7, 11, 15], 9));
+// Output [ 0, 1 ]
 
 module.exports = {
   targetSumExistsInTwoNumbers,
