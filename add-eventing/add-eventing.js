167f9ac4f967770793b0c65360669de115623c30
Leesel Fraser <leeselfraser08@gmail.com>
2020-11-20 19:58:15 -0500

    complete add-eventing

--- add-eventing/add-eventing.js
+++ add-eventing/add-eventing.js
@@ -1,4 +1,18 @@
-const addEventing = function (obj) {
+const addEventing = function (obj, name, age) {
+    this.listener = {};
+    this.name = obj.name;
+    this.age = obj.age;
+
+    this.on = function (eventName, eventHandler) {
+        this.listener[eventName] = eventHandler;
+    }
+    this.trigger = function (eventName) {
+        let eventHandler = this.listener[eventName];
+        if(eventHandler !== undefined){
+            eventHandler();
+        }
+    }
+    return this;
 }
 
 module.exports = addEventing
