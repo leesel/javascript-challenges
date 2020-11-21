fea072020d39a49a9375774bd2ba2d914f2278de
Leesel Fraser <leeselfraser08@gmail.com>
2020-11-20 20:33:18 -0500

    complete stack-machine-calculator

--- stack-machine-calculator/stack-machine-calculator.js
+++ stack-machine-calculator/stack-machine-calculator.js
@@ -1,5 +1,29 @@
+// const stackMachineCalculator = (instructions) => {
+//   return instructions
+// }
+
 const stackMachineCalculator = (instructions) => {
-  return instructions
+  const stack = [];
+  const expArray = instructions.split('');
+  for(let i = 0; i < expArray.length; i++){
+    switch (expArray[i]){
+      case "+":
+        stack.push(stack.pop() + stack.pop());
+        break;
+      case "-":
+        stack.push(stack.pop() - stack.pop());
+        break;
+      case "*":
+        stack.push(stack.pop() * stack.pop());
+        break;
+      case "/":
+        stack.push(stack.pop() / stack.pop());
+        break;
+      default:
+        stack.push(parseInt(expArray[i]));
+    }
+  }
+  return stack.pop();
 }
 
 module.exports = stackMachineCalculator
